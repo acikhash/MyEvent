775e77f6572933301c92c0cb377a0ffbbc777b3d
acikhash <157260482+acikhash@users.noreply.github.com>
2024-05-29 11:01:26 +0800

    email configuration

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'SoftUI-Laravel'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noorhasni@graduate.utm.my'),
+        'name' => env('MAIL_FROM_NAME', 'MyEvent'),
     ],
 
     /*
