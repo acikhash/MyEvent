f23a8f0c508265a3fd2dff79a0afe811dc077983
acikhash <157260482+acikhash@users.noreply.github.com>
2024-05-31 15:38:10 +0800

    edit project name

--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -3,15 +3,18 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-start">
-                    © <script>
+                    ©
+                    <script>
                         document.write(new Date().getFullYear())
                     </script>, made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>&amp; <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>
+                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>&amp;
+                    <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1"
+                        target="_blank">UPDIVISION</a>
                     for a better web.
                 </div>
             </div>
             <div class="col-lg-6">
-                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                {{-- <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
                     </li>
@@ -27,7 +30,7 @@
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
                     </li>
-                </ul>
+                </ul> --}}
             </div>
         </div>
     </div>
